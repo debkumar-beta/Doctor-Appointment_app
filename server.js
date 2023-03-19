82ee1b6f6359ca26e626a937051a479d2f409a71
Avirup Pal <avirup150@gmail.com>
2023-03-19 15:47:10 +0530

    video5 end

--- server.js
+++ server.js
@@ -3,15 +3,20 @@ const app = express();
 const colors = require("colors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 dotenv.config();
+
+//mongodb conn
+connectDB();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.get("/", (req, res) => res.send("Server is running"));
-
+// app.get("/", (req, res) => res.send("Server is running"));
+//routes
+app.use("/api/v1/user", require("./routes/userRoutes"));
 //listen
 app.listen(port, () =>
   console.log(`Doctor Appointment app listening on port ${port}!`)
